eeeaac89ac1f4730103d624c317fc84c19018a89
lobsterchan27 <lobbypolice@gmail.com>
2024-03-16 22:24:55 -0400

    some changes

--- main.py
+++ main.py
@@ -1,66 +1,60 @@
 import os
 import whisperx
 import uvicorn
+import configparser
+import tempfile
+from typing import Union
 from dotenv import load_dotenv
-from typing import List, Optional
 from fastapi import FastAPI, UploadFile, File, Form
-from pydantic import BaseModel
+from model_loader import AudioProcessor, AudioParams, Response
+from starlette.concurrency import run_in_threadpool
+
+app = FastAPI()
 
 # Load the environment variables
 load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
 
-app = FastAPI()
-
-class AudioParams(BaseModel):
-    language: str
-    segment_audio: Optional[bool] = False
-
-class Segment(BaseModel):
-    text: str
-    start: float
-    end: float
-
-class Response(BaseModel):
-    segments: List[Segment]
-    language: str
-
-# Model settings
+config = configparser.ConfigParser()
+config.read('config.ini')
 model_settings = {
-    "device": "cuda",
-    "compute_type": "float32",
-    "language": "en",
-    "whisper_arch": "large-v3",
-    "asr_options": {"word_timestamps": True}
+    "device": config.get('Model Settings', 'device', fallback='cuda'),
+    "compute_type": config.get('Model Settings', 'compute_type', fallback='float32'),
+    "language": config.get('Model Settings', 'language', fallback=None),
+    "whisper_arch": config.get('Model Settings', 'whisper_arch', fallback='large-v3'),
+    "asr_options": {"word_timestamps": config.getboolean('Model Settings', 'word_timestamps', fallback=True)},
 }
 
-# Load the model
-model = whisperx.load_model(**model_settings)
-print("Model loaded")
-# align_model, metadata = whisperx.load_align_model(language_code=model_settings["language"], device=model_settings["device"])
-# diarize_model = whisperx.DiarizationPipeline(use_auth_token=HF_TOKEN, device=model_settings["device"])
+audio_processor = AudioProcessor(model_settings,
+                                 align=config.getboolean('Model Settings', 'align'),
+                                 diarization=config.getboolean('Model Settings', 'diarize'),
+                                 HF_TOKEN=HF_TOKEN)
 
+async def save_upload_file(upload_file: UploadFile) -> str:
+    try:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(await upload_file.read())
+            return temp_file.name
+    except Exception as e:
+        print(f"Failed to save upload file: {e}")
+        return None
+    
 @app.post("/api/audio")
-def process(file: str, task_list: AudioParams=None):
-# def process(file: UploadFile=File(...), task_list: TaskList=None):
+async def process(file: Union[str, UploadFile], param: AudioParams=None):
+    if isinstance(file, UploadFile):
+        file = await run_in_threadpool(save_upload_file, file)  # run_in_threadpool is used to run sync function in async context
+    if param.language:
+        audio_processor.update_language(param.language)
     batch_size = 16
     audio = whisperx.load_audio(file)
-    result = Response(**model.transcribe(audio, batch_size=batch_size))
-    if task_list and task_list.segment_audio:
-        result = whisperx.align(result["segments"], align_model, audio, device, return_char_alignments=False)
+    result = Response(**audio_processor.model.transcribe(audio, batch_size=batch_size))
     return result
 
-def transcribe(model, video_path):
-    batch_size = 16
-    audio = whisperx.load_audio(video_path)
-    result = model.transcribe(audio, batch_size=batch_size)
-    return result
+@app.post("/api/text2speech")
+def process(text: str):
+    return "Not implemented yet"
 
 if __name__ == "__main__":
-
-    host_config = {
-    "host": "127.0.0.1",
-    "port": 8127
-    }
-
-    uvicorn.run(app, **host_config)
+    uvicorn.run(app,
+                host=config.get('Network', 'host'),
+                port=config.getint('Network', 'port'))
---
+++ model_loader.py
@@ -0,0 +1,54 @@
+from typing import Optional, List, Union
+from pydantic import BaseModel
+import whisperx
+
+class AudioParams(BaseModel):
+    language: str
+    segment_audio: Optional[bool] = False
+
+class Segment(BaseModel):
+    text: str
+    start: float
+    end: float
+
+class Response(BaseModel):
+    segments: List[Segment]
+    language: str
+
+class AudioProcessor:
+    def __init__(self, model_settings, align=False, diarization=False, HF_TOKEN=None):
+        self.model = load_whisperx(**model_settings)
+        if align:
+            self.align_model, self.metadata = load_align(model_settings["language"], model_settings["device"])
+        if diarization:
+            self.diarize_model = load_diarization(HF_TOKEN, model_settings["device"])
+
+    def process(self, file: str, param: AudioParams=None):
+        batch_size = 16
+        audio = whisperx.load_audio(file)
+        result = Response(**self.model.transcribe(audio, batch_size=batch_size))
+        return result
+    
+    def update_language(self, new_language):
+        self.model.language = new_language
+
+def load_whisperx(device: str = "cuda",
+                  compute_type: str = "float32",
+                  language: Optional[str] = None,
+                  whisper_arch: str = "large-v3",
+                  asr_options: dict = {"word_timestamps": True}):
+    
+    model = whisperx.load_model(device=device,
+                                compute_type=compute_type,
+                                language=language,
+                                whisper_arch=whisper_arch,
+                                asr_options=asr_options)
+    return model
+
+def load_align(language_code: str, device: str):
+    align_model, metadata = whisperx.load_align_model(language_code=language_code, device=device)
+    return align_model, metadata
+
+def load_diarization(use_auth_token: str, device: str):
+    diarize_model = whisperx.DiarizationPipeline(use_auth_token=use_auth_token, device=device)
+    return diarize_model
