6d177fd500b217c430505b89ba813cde8de0da50
lobsterchan27 <lobbypolice@gmail.com>
2024-04-04 17:39:01 -0400

    overlooked bugs in audio processor

--- audio_processor.py
+++ audio_processor.py
@@ -6,11 +6,16 @@ from schema import RequestParam
 
 class AudioProcessor:
     def __init__(self, model_settings, align=False, diarization=False, HF_TOKEN=None):
-        self.model = load_whisperx(model_settings)
+        self.model = None
+        self.align_model = None
+        self.diarize_model = None
+        self.metadata = None
+
+        self.load_whisperx(model_settings)
         if align:
-            self.align_model, self.metadata = load_align(model_settings["language"], model_settings["device"])
+            self.load_align(model_settings["language"], model_settings["device"])
         if diarization:
-            self.diarize_model = load_diarization(HF_TOKEN, model_settings["device"])
+            self.diarize_model = self.load_diarization(HF_TOKEN, model_settings["device"])
 
     def process(self, file: str, audio_params: RequestParam):
         batch_size = 16
@@ -21,7 +26,8 @@ class AudioProcessor:
         self.update_language(audio_params.language)
 
         result = self.model.transcribe(audio, batch_size=batch_size)
-        return result, audio
+        #TODO: alignment and diarization if needed
+        return result
     
     def set_task(self, translate: bool):
         if translate:
@@ -33,28 +39,25 @@ class AudioProcessor:
         if language:
             self.model.language = language
 
-def load_whisperx(model_settings: dict):
-    model = whisperx.load_model(device=model_settings["device"],
-                                compute_type=model_settings["compute_type"],
-                                language=model_settings["language"],
-                                whisper_arch=model_settings["whisper_arch"],
-                                asr_options=model_settings["asr_options"])
-    return model
+    def load_whisperx(self, model_settings: dict):
+        self.model = whisperx.load_model(device=model_settings["device"],
+                                    compute_type=model_settings["compute_type"],
+                                    language=model_settings["language"],
+                                    whisper_arch=model_settings["whisper_arch"],
+                                    asr_options=model_settings["asr_options"])
 
-def load_align(language_code: str, device: str):
-    align_model, metadata = whisperx.load_align_model(language_code=language_code, device=device)
-    return align_model, metadata
+    def load_align(self, language_code: str, device: str):
+        self.align_model, self.metadata = whisperx.load_align_model(language_code=language_code, device=device)
 
-def load_diarization(use_auth_token: str, device: str):
-    diarize_model = whisperx.DiarizationPipeline(use_auth_token=use_auth_token, device=device)
-    return diarize_model
+    def load_diarization(self, use_auth_token: str, device: str):
+        self.diarize_model = whisperx.DiarizationPipeline(use_auth_token=use_auth_token, device=device)
 
-def clean_up(self):
-    gc.collect()
-    torch.cuda.empty_cache()
-    if hasattr(self, 'model'):
-        del self.model
-    if hasattr(self, 'align_model'):
-        del self.align_model
-    if hasattr(self, 'diarize_model'):
-        del self.diarize_model
+    def clean_up(self):
+        gc.collect()
+        torch.cuda.empty_cache()
+        if hasattr(self, 'model'):
+            del self.model
+        if hasattr(self, 'align_model'):
+            del self.align_model
+        if hasattr(self, 'diarize_model'):
+            del self.diarize_model
