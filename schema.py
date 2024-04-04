6e8e8d171bbfd2c5e6f5a65b8206bca035b2d56e
lobsterchan27 <lobbypolice@gmail.com>
2024-04-03 23:07:08 -0400

    savepath dataclass, response

--- schema.py
+++ schema.py
@@ -1,5 +1,10 @@
-from typing import Optional
+import io
+
+from typing import Optional, Tuple, AsyncGenerator
 from pydantic import BaseModel
+from fastapi import Response
+from fastapi.responses import StreamingResponse
+from dataclasses import dataclass
 
 class RequestParam(BaseModel):
     language: str = None
@@ -15,3 +20,31 @@ class TTSParam(BaseModel):
     regenerate: Optional[str] = None
     seed: Optional[int] = None
     kv_cache: bool = True
+
+class MultipartResponse(Response):
+    media_type = "multipart/form-data"
+    boundary = "--bulk-data-boundary"
+
+    async def render(self, content: AsyncGenerator[Tuple[str, bytes], None]):
+        for data_type, data in content:
+            yield (
+                f"{self.boundary}\r\n"
+                f"Content-Type: {data_type}\r\n\r\n"
+            ).encode()
+            yield data
+            yield "\r\n".encode()
+        yield f"{self.boundary}--".encode()
+
+    def __call__(self, content: AsyncGenerator[Tuple[str, bytes], None]):
+        return StreamingResponse(
+            self.render(content),
+            headers={
+                "Content-Type": f"multipart/form-data; boundary={self.boundary[2:]}"
+            },
+        )
+
+@dataclass
+class SavePath:
+    audio: str
+    json: str
+    video: str = None
